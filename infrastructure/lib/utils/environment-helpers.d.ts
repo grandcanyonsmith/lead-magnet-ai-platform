3df4ed694dfece08054d44e00c701865c0fc7548
Canyon Smith <canyonfsmith@gmail.com>
2025-11-08 07:57:54 -0700

    Refactor lambda-helpers to use aws-logs module and improve error handling in dependency tests

---
+++ infrastructure/lib/utils/environment-helpers.d.ts
@@ -0,0 +1,5 @@
+import { TableMap } from '../types';
+/**
+ * Creates environment variables from a tables map
+ */
+export declare function createTableEnvironmentVars(tablesMap: TableMap): Record<string, string>;
